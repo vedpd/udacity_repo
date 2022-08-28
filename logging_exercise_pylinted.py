1ad8058f6f2ea4eb82ca368475aadc608f2b3a8b
VED PRAKASH DWIVEDI <ved.30dec@gmail.com>
2022-08-28 13:03:37 +0530

    Add files via upload
    Logging, try-except, assestion, pylint and autopep8

--- logging_exercise_pylinted.py
+++ logging_exercise_pylinted.py
@@ -1,24 +1,62 @@
-## STEPS TO COMPLETE ##
-# 1. import logging
-# 2. set up config file for logging called `results.log`
-# 3. add try except with logging for success or error
-#    in relation to checking the types of a and b
-# 4. check to see that log is created and populated correctly
-#    should have error for first function and success for
-#    the second
-# 5. use pylint and autopep8 to make changes
-#    and receive 10/10 pep8 score
+'''
+Testing & Logging exercise solution
 
-def sum_vals(a, b):
+author: Josh
+date: March 27, 2021
+'''
+
+import logging
+logging.basicConfig(
+    filename='./test_results.log',
+    level=logging.INFO,
+    filemode='w',
+    format='%(name)s - %(levelname)s - %(message)s')
+
+
+def divide_vals(numerator, denominator):
     '''
     Args:
-        a: (int)
-        b: (int)
-    Return:
-        a + b (int)
+        numerator: (float) numerator of fraction
+        denominator: (float) denominator of fraction
+
+    Returns:
+        fraction_val: (float) numerator/denominator
     '''
-    return a+b
+    try:
+        assert denominator != 0
+        assert isinstance(numerator, float)
+        assert isinstance(denominator, float)
+        fraction_val = numerator / denominator
+        logging.info("Testing divide_vals: SUCCESS")
+        return fraction_val
+    except AssertionError:
+        logging.error(
+            "Testing divide_vals: The type needs to be float and denominator cannot be 0.")
+
+
+
+def num_words(text):
+    '''
+    Args:
+        text: (string) string of words
+
+    Returns:
+        num_words: (int) number of words in the string
+    '''
+    try:
+        assert isinstance(text, str)
+        total_words = len(text.split())
+        logging.info("Testing num_words: SUCCESS")
+        return total_words
+    except AssertionError:
+        logging.error("Testing num_words: The type needs to be a string")
+
 
 if __name__ == "__main__":
-    sum_vals('no', 'way')
-    sum_vals(4, 5)
+    divide_vals(3.4, 0)
+    divide_vals(4.5, 2.7)
+    divide_vals(-3.8, 2.1)
+    divide_vals(1, 2)
+    num_words(5)
+    num_words('This is the best string')
+    num_words('one')
